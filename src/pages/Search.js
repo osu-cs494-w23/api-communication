99d3b96a336f37ce20afd8b613bbee2ee73f3baa
Rob Hess <hessro@oregonstate.edu>
2023-02-06 15:23:42 -0800

    Add code from 2/6 lecture.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSearchParams } from 'react-router-dom'
 
 function Search({ query }) {
     const [ inputQuery, setInputQuery ] = useState(query || "")
     const [ searchParams, setSearchParams ] = useSearchParams()
+
+    const [ repos, setRepos ] = useState([])
+
+    // console.log("== repos:", repos)
+
+    useEffect(() => {
+        let ignore = false
+        const controller = new AbortController()
+        async function fetchSearchResults() {
+            let responseBody = {}
+            try {
+                const response = await fetch(
+                    `https://api.github.com/search/repositories?q=${query}&sort=stars`,
+                    { signal: controller.signal }
+                )
+                responseBody = await response.json()
+            } catch (e) {
+                if (e instanceof DOMException) {
+                    console.log("HTTP request cancelled")
+                } else {
+                    throw e
+                }
+            }
+
+            if (!ignore) {
+                setRepos(responseBody.items || [])
+            }
+        }
+        if (query) {
+            fetchSearchResults()
+        }
+        return () => {
+            ignore = true
+            controller.abort()
+        }
+    }, [ query ])
 
     return (
         <div>
@@ -15,6 +51,13 @@ function Search({ query }) {
                 <button type="submit">Search</button>
             </form>
             <h2>Search query: {query}</h2>
+            <ul>
+                {repos.map(repo => (
+                    <li key={repo.id}>
+                        <a href={repo.html_url}>{repo.full_name}</a>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
